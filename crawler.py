df3233259021e413c3e60dcd0b1ec82c4aa86372
Andrew Wang <wardengnaw@gmail.com>
2015-11-30 19:42:41 -0800

    adding afinn sentiment analysis and more information to JSON object
    Adding ASIN, product title, customerId

--- crawler.py
+++ crawler.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import os
 
 from bs4 import BeautifulSoup
 import urllib2
@@ -11,17 +12,23 @@ from selenium import webdriver
 import time
 import argparse
 from random import randint
-
+from afinn import Afinn
+afinn = Afinn()
 
 class Review:
-    def __init__(self, title, reviewText, author, reviewDate, reviewComments, stars, helpfulVotes):
-        self.title = title
+    def __init__(self, asin, productName, reviewTitle, reviewText, author, authorId, reviewDate, badge, reviewComments, stars, helpfulVotes, sentiment):
+        self.asin = asin
+        self.productName = productName
+        self.reviewTitle = reviewTitle
         self.reviewText = reviewText
         self.author = author
+        self.authorId = authorId
+        self.badge = badge.text if badge else ""
         self.reviewDate = reviewDate
         self.reviewComments = reviewComments
         self.stars = stars
         self.helpfulVotes = helpfulVotes
+        self.sentiment = sentiment
 
     def __str__(self):
        return json.dumps(self.__dict__)
@@ -49,10 +56,10 @@ def getNumStars(review):
 def getHelpfulVotes(review):
    votes = review.find('span', {"class" : "review-votes"})
    if (votes):
-       votes_ = votes.text.split(' ')
+       votes_ = votes.text.replace(',', '').split(' ')
        return {'num_helpful': int(votes_[0]), 'num_total': int(votes_[2]), 'pct_helpful': float(votes_[0])/int(votes_[2])}
    else:
-       return {'num_helpful': None, 'num_total': None, 'pct_helpful': None}
+       return {'num_helpful': 0, 'num_total': 0, 'pct_helpful': float('nan')}
 
 def getReviewComments(reviewComments):
    # TODO: Do something meaningful with sub comments
@@ -90,6 +97,7 @@ def getProductReviews(asin, subcomments, pageLimit):
 
    reviews = []
    bs = BeautifulSoup(browser.page_source)
+   productName = bs.find('a', {"class" : "a-size-large a-link-normal"}).text.encode('utf-8')
    maxPageNum = 0
 
    # Find number of review pages
@@ -103,20 +111,30 @@ def getProductReviews(asin, subcomments, pageLimit):
 
    #Loop through all the review pages and get the reviews
    idx = 1
+   print "Max Page " + str(maxPageNum)
+   print "Page Limit" + str(pageLimit)
    while (idx < (maxPageNum + 1) and idx < pageLimit):
+      print "Index: " + str(idx) 
       for x in bs.findAll('div', {"class" : "review"}):
-          review = Review(x.find('a', {"class" : "review-title"}).text.encode('utf-8'),
+          review = Review(asin,
+                         productName,
+                         x.find('a', {"class" : "review-title"}).text.encode('utf-8'),
                          x.find('span', {"class" : "review-text"}).text.encode('utf-8'),
                          x.find('a', {"class" : "author"}).text.encode('utf-8'),
+                         x.find('a', {"class" : "author"})['href'].split('/')[4],
                          x.find('span', {"class" : "review-date"}).text.encode('utf-8'),
+                         x.find('span', {"class" : "a-size-mini a-color-link c7yBadgeAUI c7yTopDownDashedStrike c7y-badge-text a-text-bold"}),
                          None,
                          getNumStars(x),
-                         getHelpfulVotes(x))
+                         getHelpfulVotes(x),
+                         afinn.score(x.find('span', {"class" : "review-text"}).text.encode('utf-8')),
+                         )
           reviews.append(review)
 
       url = "http://www.amazon.com/product-reviews/" + str(asin) + "/?showViewpoints=0&sortBy=byRankDescending&pageNumber=" + str(idx)
       browser.get(url)
       if subcomments:
+          print "getting subcomments"
           for link in browser.find_elements_by_xpath("//a[contains(concat(' ',normalize-space(@class),' '),' a-link-expander ')]"):
              link.click()
              # Wait between 5 to 20 seconds between clicks
@@ -124,7 +142,7 @@ def getProductReviews(asin, subcomments, pageLimit):
       bs = BeautifulSoup(browser.page_source)
       idx += 1
       # Wait 30 to 90 seconds per review page
-      time.sleep(randint(30, 90))
+      time.sleep(randint(10, 20))
 
    browser.close()
 
@@ -136,7 +154,7 @@ def main():
    parser.add_argument('-o', action="store", dest="output", help="Output results to a file")
    parser.add_argument('-subcomments', action="store_true", default=False, dest="subcomments", help="Enable reading comment's comments")
    parser.add_argument('-asins', action='append', default=[], dest='asins', help="List of asins to scrape")
-   parser.add_argument('-n', action='store', default=5, dest='numPages', help="Number of pages to read (default 5)")
+   parser.add_argument('-n', action='store', default=5, dest='numPages', type=int, help="Number of pages to read (default 5)")
 
    results, unknown = parser.parse_known_args()
 
