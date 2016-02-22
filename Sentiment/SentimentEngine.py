13f9dfc8fb982589b446dfdee9b6df9faf71dac0
Andrew Wang <wardengnaw@gmail.com>
2016-02-21 17:13:54 -0800

    Adding GetPolarity(word) API
    Very basic at the moment. Just checks to see if a negation word appeared before it.
    Will need to handle words such as just kidding which may cause a reverse negation of
    the whole sentence

--- Sentiment/SentimentEngine.py
+++ Sentiment/SentimentEngine.py
@@ -1,20 +1,30 @@
 import sys
 import nltk
 import SetupSentimentData
+from nltk.tokenize import TweetTokenizer
 
 # Set up word lists to use for sentiment analysis.
 negativeWordsList = SetupSentimentData.setupNegativeWordList()
-positiveWordsList = SetupSentimentData.setupPositiveWordList()
-anewWordList = SetupSentimentData.setupANEWWordList()
+positiveWordsList = SetupSentimentData.setupPositiveWordList() 
+anewWordList = SetupSentimentData.setupANEWWordList() 
 sentiWordNetList = SetupSentimentData.setupSentiWordNetList()
+negationWords = ["no", "not", "doesn't", "neither"]
 
 class SentimentEngine:
     def __init__(self, sentence):
         self.rawSentence = sentence
-        self.tokens = nltk.word_tokenize(sentence)
+        self.tokens = TweetTokenizer().tokenize(sentence)
+
+        # Setup basic polarity
+        self.polarity = []
+        curPolarity = 1
+        for token in self.tokens:
+            if token in negationWords:
+                curPolarity = curPolarity * -1
+            self.polarity.append(curPolarity)
+
         self.pos = [pos[1] for pos in nltk.pos_tag(self.tokens)]
-        self.wordsSentiment = [self.getWordSentiment(word) for word in self.tokens]
-        print self.wordsSentiment
+        self.wordsSentiment = [self.getWordSentiment(word) * self.getPolarity(word) for word in self.tokens]
         self.aggregiateSentenceSentiment = sum(self.wordsSentiment)
         self.absSentenceSentiment = max(abs(i) for i in self.wordsSentiment)
         self.maxSentenceSentiment = max(self.wordsSentiment)
@@ -31,8 +41,7 @@ class SentimentEngine:
         return 0
 
     def getAggregiateSentenceSentiment(self):
-        return self.aggregiateSentenceSentiment
-
+        return self.aggregiateSentenceSentiment 
     def getAbsSentenceSentiment(self):
         return self.absSentenceSentiment
 
@@ -42,5 +51,5 @@ class SentimentEngine:
     def getMinSentenceSentiment(self):
         return self.minSentenceSentiment
 
-    def getPolarity(word):
-        return 0
+    def getPolarity(self, word):
+        return self.polarity[self.tokens.index(word)]
